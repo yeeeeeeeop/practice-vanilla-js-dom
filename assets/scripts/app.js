31e4a1113f238c9aa5bd281528348f8ef98c9d84
yeeeeeeeop <yeop0916@gmail.com>
2024-02-11 21:27:27 +0900

    feat: 모달 on/off 시 input 초기화 & 모달 내 Add 누를 시 movies 배열에 추가

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,9 +5,19 @@ const addMovieModalEl = document.getElementById('add-modal');
 // buttons
 const startAddMovieButtonEl = document.querySelector('header button');
 const cancelAddMovieButtonEl = addMovieModalEl.querySelector('.btn--passive');
+const confirmAddMovieButtonEl = addMovieModalEl.querySelector('.btn--success');
 
 // overlay
 const backdropEl = document.getElementById('backdrop');
+
+// input
+// const inputTitleEl = document.getElementById('title');
+// const inputImageUrlEl = document.getElementById('image-url');
+// const inputRatingEl = document.getElementById('rating');
+const inputEls = document.querySelectorAll('input');
+
+// ---------- variables ----------
+const movies = [];
 
 // ---------- handlers ----------
 const toggleBackdrop = () => {
@@ -19,15 +29,51 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
-const cancelAddMovie = () => {
+const backdropClickHandler = () => {
   toggleMovieModal();
 };
 
-const backdropClickHandler = () => {
+const clearMovieInput = () => {
+  for (const inputEl of inputEls) {
+    inputEl.value = '';
+  }
+};
+
+const cancelAddMovieHandler = () => {
   toggleMovieModal();
+  clearMovieInput();
+};
+
+const addMovieHandler = () => {
+  const titleValue = inputEls[0].value;
+  const imageUrlValue = inputEls[1].value;
+  const ratingValue = inputEls[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert('Please enter valid values (rating between 1 and 5).');
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  clearMovieInput();
 };
 
 // ---------- event listeners ----------
 startAddMovieButtonEl.addEventListener('click', toggleMovieModal);
 backdropEl.addEventListener('click', backdropClickHandler);
-cancelAddMovieButtonEl.addEventListener('click', cancelAddMovie);
+cancelAddMovieButtonEl.addEventListener('click', cancelAddMovieHandler);
+confirmAddMovieButtonEl.addEventListener('click', addMovieHandler);
