b394530bc95050ea8beb0d7658aaf03218ebdb26
yeeeeeeeop <yeop0916@gmail.com>
2024-02-11 21:01:05 +0900

    feat: add moive 모달 내 cancel 누를 시 모달 off

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,10 @@
 // ---------- elements ----------
-// buttons
-const startAddMovieButtonEl = document.querySelector('header button');
-
 // modals
 const addMovieModalEl = document.getElementById('add-modal');
+
+// buttons
+const startAddMovieButtonEl = document.querySelector('header button');
+const cancelAddMovieButtonEl = addMovieModalEl.querySelector('.btn--passive');
 
 // overlay
 const backdropEl = document.getElementById('backdrop');
@@ -18,4 +19,15 @@ const toggleMovieModal = () => {
   toggleBackdrop();
 };
 
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+// ---------- event listeners ----------
 startAddMovieButtonEl.addEventListener('click', toggleMovieModal);
+backdropEl.addEventListener('click', backdropClickHandler);
+cancelAddMovieButtonEl.addEventListener('click', cancelAddMovie);
